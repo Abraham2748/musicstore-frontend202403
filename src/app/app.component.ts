154bb8630d051cd2d4ae44fa5e010666169a91a8
Abraham Salazar <abraham_jack10@hotmail.com>
2024-07-15 17:05:37 -0500

    Use every page component in App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    EventDetailComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

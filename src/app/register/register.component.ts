5b4e5691184bf94da847d1d3688f1f3d2ef4ffd1
Abraham Salazar <abraham_jack10@hotmail.com>
2024-07-15 17:04:15 -0500

    Update Register component UI

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { SimpleHeaderComponent } from '../shared/components/simple-header/simple-header.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [SimpleHeaderComponent, FooterComponent],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent {}

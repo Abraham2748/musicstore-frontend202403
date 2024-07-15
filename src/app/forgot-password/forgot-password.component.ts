2c15018d83f322a09b338ded7c9ae58227d091a9
Abraham Salazar <abraham_jack10@hotmail.com>
2024-07-15 17:04:34 -0500

    Update Forgot Password component UI

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { SimpleHeaderComponent } from '../shared/components/simple-header/simple-header.component';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [],
+  imports: [SimpleHeaderComponent, FooterComponent],
   templateUrl: './forgot-password.component.html',
-  styleUrl: './forgot-password.component.css'
+  styleUrl: './forgot-password.component.css',
 })
-export class ForgotPasswordComponent {
-
-}
+export class ForgotPasswordComponent {}

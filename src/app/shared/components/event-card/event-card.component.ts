376beee8d583705a3e43aeed856b57bf0cc112e5
Abraham Salazar <abraham_jack10@hotmail.com>
2024-07-17 20:42:32 -0500

    Apply textLimiter pipe in Event Card

--- src/app/shared/components/event-card/event-card.component.ts
+++ src/app/shared/components/event-card/event-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Concert, emptyConcert } from '../../models/concert.model';
+import { TextLimiterPipe } from '../../pipes/text-limiter.pipe';
 
 @Component({
   selector: 'app-event-card',
   standalone: true,
-  imports: [],
+  imports: [TextLimiterPipe],
   templateUrl: './event-card.component.html',
   styleUrl: './event-card.component.css',
 })

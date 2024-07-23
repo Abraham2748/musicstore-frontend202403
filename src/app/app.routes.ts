7df906ea38cd57f301b38570cd229914b92270c1
Abraham Salazar <abraham_jack10@hotmail.com>
2024-07-22 21:52:05 -0500

    Add forgot-password component to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   {
@@ -23,5 +20,12 @@ export const routes: Routes = [
     path: 'register',
     loadComponent: () =>
       import('./register/register.component').then((m) => m.RegisterComponent),
+  },
+  {
+    path: 'forgot-password',
+    loadComponent: () =>
+      import('./forgot-password/forgot-password.component').then(
+        (m) => m.ForgotPasswordComponent
+      ),
   },
 ];

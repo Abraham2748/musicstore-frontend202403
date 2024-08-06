7d525dc387645e5f5edd012e804e9bcd9452a964
Abraham Salazar <abraham_jack10@hotmail.com>
2024-08-05 21:03:35 -0500

    Implement pie chart using ng2-charts in reports component

--- src/app/admin/reports/reports.component.ts
+++ src/app/admin/reports/reports.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.css',
 })
-export class ReportsComponent {}
+export class ReportsComponent {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  pieChartData: ChartData<'pie', number[], string> = {
+    labels: ['Ventas', 'Compras', 'Ganancias'],
+    datasets: [
+      {
+        data: [500, 300, 200],
+      },
+    ],
+  };
+  pieChartType: ChartType = 'pie';
+
+  loadRealData() {
+    this.pieChartData.datasets[0].data = [100, 200, 300];
+    this.chart?.update();
+  }
+}
